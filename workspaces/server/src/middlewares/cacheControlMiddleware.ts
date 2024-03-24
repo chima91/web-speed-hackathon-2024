58361baba8fd054d7515859aeed9f606bfb9768d
chima91 <itb4769we@gmail.com>
2024-03-24 16:45:14 +0900

    feat(/): set Cache-Control header for /assets

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -2,5 +2,10 @@ import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
+
+  if (new URL(c.req.url).pathname.startsWith('/assets/')) {
+    c.res.headers.set('Cache-Control', 'public, max-age=31536000');
+    return;
+  }
   c.res.headers.set('Cache-Control', 'private, no-store');
 });
