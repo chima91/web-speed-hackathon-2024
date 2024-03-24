8d16e63b3d71457baa8b2f5584adb202d34366d1
chima91 <itb4769we@gmail.com>
2024-03-24 14:57:04 +0900

    feat(/): tweak SearchPage

--- workspaces/app/src/pages/SearchPage/index.tsx
+++ workspaces/app/src/pages/SearchPage/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useCallback, useEffect, useId, useState } from 'react';
+import { memo, Suspense, useCallback, useId, useState } from 'react';
 
 import { useBookList } from '../../features/book/hooks/useBookList';
 import { Box } from '../../foundation/components/Box';
@@ -8,33 +8,27 @@ import { Color, Space, Typography } from '../../foundation/styles/variables';
 import { Input } from './internal/Input';
 import { SearchResult } from './internal/SearchResult';
 
+const MemoizedSearchResult = memo(SearchResult);
+
 const SearchPage: React.FC = () => {
   const { data: books } = useBookList({ query: {} });
 
   const searchResultsA11yId = useId();
 
-  const [isClient, setIsClient] = useState(false);
   const [keyword, setKeyword] = useState('');
 
-  const onChangedInput = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setKeyword(event.target.value);
-    },
-    [setKeyword],
-  );
-
-  useEffect(() => {
-    setIsClient(true);
+  const onChangedInput = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
   }, []);
 
   return (
     <Box px={Space * 2}>
-      <Input disabled={!isClient} onChange={onChangedInput} />
+      <Input onChange={onChangedInput} />
       <Box aria-labelledby={searchResultsA11yId} as="section" maxWidth="100%" py={Space * 2} width="100%">
         <Text color={Color.MONO_100} id={searchResultsA11yId} typography={Typography.NORMAL20} weight="bold">
           検索結果
         </Text>
-        {keyword !== '' && <SearchResult books={books} keyword={keyword} />}
+        {keyword !== '' && <MemoizedSearchResult books={books} keyword={keyword} />}
       </Box>
     </Box>
   );
@@ -42,7 +36,7 @@ const SearchPage: React.FC = () => {
 
 const SearchPageWithSuspense: React.FC = () => {
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<div>Loading...</div>}>
       <SearchPage />
     </Suspense>
   );
